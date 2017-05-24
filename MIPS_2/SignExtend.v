e322b4f4731f90ec11f660ec1183b912256b301e
wwqqqqq <weiq618@gmail.com>
2017-05-24 13:24:47 +0800

    remove temp file

--- MIPS_2/SignExtend.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module SignExtend(
-input [31:0] Instr,
-output [31:0] SignImm
-    );
-assign SignImm=(Instr[15]==1'b1)?(32'hffff0000|Instr):(32'h0000ffff&Instr);
-
-endmodule
